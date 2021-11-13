26f39c08d09201d0ef2c99d2f9f0f8a5c0525d2e
JulienT91 <julien.tavet30@gmail.com>
2021-11-13 14:16:34 +0100

    redesign Details componant,add padding and margin 0 to setting

--- src/view/Details.jsx
+++ src/view/Details.jsx
@@ -20,52 +20,86 @@ function Details() {
     },[]);
     
    
-    if(!movieDetails) return <p>Error, nothing found..</p>
+    if(!movieDetails) return <p>error, nothing found..</p>
     return (    // image,title,overview
-                <div className="details_container">
-                  <div className="movie__cont">
-                    <div className="zoom__img">
-                    <div className="details__info">
-                      <h2>Catégories:{movieDetails.categories.join()}</h2>
-                      <h3>{movieDetails.title}</h3>
-                      <h3>{DateFunction.DateForm(movieDetails.release_date)}</h3>
-                    </div>
-                      <img className="card-img-transition" src={movieDetails.poster} alt={movieDetails.title} />
-                    </div>
-                      <Buttons id={movieDetails.id}  />
-                    <div className="details__desc">
-                       <h4>Synopsis:</h4>
-                      <p>{movieDetails.description}</p>
-                    </div>
-                  </div>
-                  <h2 className="similar_title">Acteurs:</h2>
-                  <div className="actor__container">
-                     {movieDetails && movieDetails.actors.map(({name,photo,character},index) => (
-                       <div  key={index} className="details_group">
-                        <div className="details_wrapper">
-                          <h3>Acteur:{name}</h3>
-                          <h4>Rôle:{character}</h4>
-                          <img className="card-img-transition" src={photo} alt={name} />
-                        </div>
-                       </div>
-                     ))}
-                    </div>
-                    <h2 className="similar_title">Film Similaires:</h2>
-                    <div className="similar__container">
-                      {movieDetails && movieDetails.similar_movies.map(({title,poster,release_date},index) => (
-                       <div  key={index} className="details_group">
-                        <div className="details_wrapper">
-                          <img className="card-img-transition" src={poster} alt={title} />
-                          <h3>Titre:{title}</h3>
-                          <h3>Date de sortie:{DateFunction.DateForm(release_date)}</h3>
-                        </div>
-                       </div>
-                     ))}
-                     </div>
-                   </div>
-      
-    )
-}
-
-export default Details
+      <>
+      <div className="details__wrapper">
+        <div className="details__header">
+          <h1>{movieDetails.title}</h1>
+        </div>
+        <div className="details__card">
+          <figure className="thumbnail">
+            <img className="thumbnail__img" src={movieDetails.poster} alt={movieDetails.title} />
+          </figure>
+          <div className="meta">
+            <div className="meta__body">
+              <div className="meta__body__info__release">
+                <p><span className="grey_text">sortie le : </span>{DateFunction.DateForm(movieDetails.release_date)}</p>
+              </div>
+              <div className="meta__body__info__categories">
+                <p><span className="grey_text">Catégories : </span>{movieDetails.categories.join()}</p>
+              </div>
+              <div className="meta__body__button">
+                <Buttons />
+              </div>
+            </div>
+          </div>
+        </div>
+        <section className="details__synposis">
+          <div className="titleBar">
+            <h2>synposis</h2>
+          </div>
+          <div className="synopsis__text">
+            {movieDetails.description}
+          </div>
+        </section>
+        <section className="details__actors">
+          <div className="titleBar">
+            acteurs et actrices
+          </div>
+          {movieDetails && movieDetails.actors.map(({name,photo,character},index) => (
+            <div key={index} className="card__actors">
+              <figure className="thumbnail">
+                <img className="thumbnail__img" src={photo} alt={name} />
+              </figure>
+              <div className="meta">
+                <div className="meta__title">
+                  <h3>
+                    {name}
+                  </h3>
+                </div>
+                <div className="meta__role">
+                  <p>Rôle : {character}</p>
+                </div>
+              </div>
+            </div>
+            ))}
+        </section>
+        <section className="details__similar">
+          <div className="titleBar">
+              films similaires
+          </div>
+           {movieDetails && movieDetails.similar_movies.map(({title,poster,release_date},index) => (
+             <div key={index} className="card__similar">
+                <figure className="thumbnail">
+                <img className="thumbnail__img" src={poster} alt={title} />
+              </figure>
+              <div className="meta">
+                <div className="meta__title">
+                  <h3>
+                    {title}
+                  </h3>
+                </div>
+                <div className="meta__release_date">
+                  <p>{DateFunction.DateForm(release_date)}</p>
+                </div>
+              </div>
+             </div>        
+            ))}
+        </section>
+      </div>
+      </>  
+  )
+    }
+export default Details;
  
