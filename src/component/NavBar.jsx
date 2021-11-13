93ee0cb80423eeca10b37d6bb7dab9bd5eab9c2b
JulienT91 <julien.tavet30@gmail.com>
2021-11-13 18:55:51 +0100

    update navbar component

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,27 +1,51 @@
-import React from "react";
+import React,{ useState }from "react";
 import { NavLink } from "react-router-dom";
+import {FaBars} from "react-icons/fa";
 
 
 const NavBar = () => {
-    return (
-    <div className="navBar-container">
-      <nav className="nav_contents">
-        <h1 className="nav_title">MoviesBoard</h1>
-        <ul className="navigation">
-          <li>
-            <NavLink exact to="/" activeClassName="nav-active">
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const Close = () => setClick(false);
+return (
+  <div>
+   <div className={click ? "main-container" : ""} onClick={()=>Close()} />
+    <nav className="navbar" onClick={(e)=>e.stopPropagation()}>
+      <div className="nav-container">
+        <h1>moviesboard</h1>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <NavLink
+              exact
+              to="/"
+              activeClassName="active"
+              className="nav-links"
+              onClick={click ? handleClick : null}
+            >
               Accueil
             </NavLink>
           </li>
-          <li>
-            <NavLink exact to="/add" activeClassName="nav-active">
-                Ajouter un film
+          <li className="nav-item">
+            <NavLink
+              exact
+              to="/add"
+              activeClassName="active"
+              className="nav-links"
+              onClick={click ? handleClick : null}
+            >
+              Ajouter un film
             </NavLink>
           </li>
         </ul>
-       </nav>
+        <div className="nav-icon" onClick={handleClick}>
+          <i className={click ? "fa fa-times" : "fa fa-bars"}><FaBars /></i>
+        </div>
       </div>
-    );
-  };
+    </nav>
+  </ div>
+);
+}
+
+ 
   
   export default NavBar;
